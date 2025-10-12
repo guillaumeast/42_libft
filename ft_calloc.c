8bcd6e82e9f82a4fb408c2fbeb85e823b36d2cef
Guillaume A <guillaume.astesana@gmail.com>
2025-10-12 18:24:56 +0200

    added calloc

---
+++ ft_calloc.c
@@ -0,0 +1,20 @@
+/*
+Contiguously allocates enough space for count objects that are size bytes of memory each and returns a pointer to the allocated memory.
+The allocated memory is filled with bytes of value zero.
+*/
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t	i;
+	void	*res;
+
+	if (size != 0 && count > SIZE_MAX / size)
+		return (NULL);
+	res = malloc(count * size);
+	if (!res)
+		return (NULL);
+	ft_bzero(res, count * size);
+	return (res);
+}
