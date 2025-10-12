af668bcdfe047c87419be12df33478c2711f780a
Guillaume A <guillaume.astesana@gmail.com>
2025-10-12 23:02:50 +0200

    added ft_lstsize

---
+++ ft_lstsize.c
@@ -0,0 +1,18 @@
+/*
+Counts the number of nodes in a list.
+*/
+
+#include "libft.h"
+
+int		ft_lstsize(t_list *lst)
+{
+	int	size;
+
+	size = 0;
+	while (lst)
+	{
+		size++;
+		lst = lst->next;
+	}
+	return (size);
+}
