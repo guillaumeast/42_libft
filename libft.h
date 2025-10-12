420694065af895926ba356f68290d48311af9d63
Guillaume A <guillaume.astesana@gmail.com>
2025-10-12 22:05:33 +0200

    added itoa

--- libft.h
+++ libft.h
@@ -3,6 +3,7 @@
 
 # include <ctype.h>		// For size_t
 # include <stdlib.h>	// For NULL and malloc()
+# include <stdio.h>		// For printf (tests only)
 
 // Part 1
 int		ft_isalpha(int c);
@@ -34,5 +35,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
