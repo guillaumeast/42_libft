b34dec14ebb8aed0fda01e36727ff10728dca13b
Guillaume A <guillaume.astesana@gmail.com>
2025-10-12 17:06:20 +0200

    added strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,5 +1,5 @@
 /*
-Locates the first occurrence of c (converted to a char) in the string pointed to by s.
+Locates the last occurrence of c (converted to a char) in the string pointed to by s.
 The terminating null character is considered part of the string.
 Therefore if c is ‘\0’, the functions locate the terminating ‘\0’.
 ---
@@ -8,18 +8,20 @@ Returns a pointer to the located character, or NULL if the character does not ap
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
+	char	*last;
 	char	target;
 
+	last = NULL;
 	target = (char) c;
 	while (*s)
 	{
 		if (*s == target)
-			return ((char *) s);
+			last = (char *) s;
 		s++;
 	}
 	if (target == '\0')
-		return ((char *) s);
-	return (NULL);
+		last = (char *) s;
+	return (last);
 }
