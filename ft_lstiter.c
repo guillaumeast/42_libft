ff09dd553fe448367dd1aa9de4406a39d2cd5c38
Guillaume A <guillaume.astesana@gmail.com>
2025-10-12 23:29:32 +0200

    added ft_lstiter

---
+++ ft_lstiter.c
@@ -0,0 +1,19 @@
+/*
+Iterates the list ’lst’ and applies the function ’f’ on the content of each node.
+*/
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*tmp;
+
+	if (!lst || !f)
+		return ;
+	tmp = lst;
+	while (tmp)
+	{
+		f(tmp->content);
+		tmp = tmp->next;
+	}
+}
